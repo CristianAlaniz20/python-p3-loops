fbc42c0f1b431a0ba3f2596cb4ab5615cd2de4f2
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-03-31 20:04:02 -0700

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        if i == 1:
+            print("Happy New Year!")
+        i -= 1
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = [number ** 2 for number in int_list]
+    return squared_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(f"{i}")
